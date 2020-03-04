1b18c5e359d775191ddd25ea81de2537eb17b950
windxuan <web711huhaoxuan@163.com>
2020-03-04 19:23:29 +0800

    css3Loading: The animation page

---
+++ loading.js
@@ -0,0 +1,20 @@
+var per = 0;
+
+var timer = setInterval(function(){
+    $('.bar').css('width',per + "%");
+    per += 1;
+    if(per>100){
+        $('.pageLoading').addClass('complete');
+        setTimeout(function(){
+            $('.mosterText').html("<h2>\
+            We are\
+            <br>\
+            SQUARE\
+            <br>\
+            MONSTER!\
+        </h2>\
+        <h3>Ahhhh We'll eat you.</h3>");
+        },1000);
+        clearInterval(timer);
+    }
+},30)
